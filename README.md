cf2ea1bd4b4318b23771d05ab90079c476ab16c5
Jon Patterson <jonpatterson@users.noreply.github.com>
2020-05-12 21:31:13 +0100

    Add keyboard shortcuts (#7)
    * Toggle Tumber in current window with Ctrl+Space
    * Toggle Tumber in all windows with Ctrl+Shift+Space
    * Updated README with instructions and general housekeeping

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # Tumber #
-
 Google Chrome allows fast switching of tabs using keyboard shortcuts.  With no tab numbers displayed and several tabs open this can take a bit of guess work!  Tumber is a Chrome Extension that displays tab numbers for tab jumping ninja!
 
 ### Setup ###
-
 1. Clone or download this repository
 2. Launch Google Chrome and enter `chrome://extensions` in the address bar
 3. Check the option to enable 'Developer mode'
 4. Click the 'Load unpacked' button and select `src` in the project directory
 
 ### Usage ###
-
 1. Launch Google Chrome and open several tabs containing webpages
-2. Right-click anywhere in an open tab and click "Tumber" in the context menu
+2. Right-click anywhere in an open tab and select "Tumber" then "Show" from the context menu
 3. Et voila, Tumber will add a tab number to the start of each tab title
-4. Press <kbd>&#8984;</kbd> + <kbd>1</kbd> through <kbd>&#8984;</kbd> + <kbd>8</kbd> to jump to a tab on Mac
-5. Press <kbd>Ctrl</kbd> + <kbd>1</kbd> through <kbd>Ctrl</kbd> + <kbd>8</kbd> to jump to a tab on Windows
+4. Press <kbd>&#8984;</kbd>+<kbd>1</kbd> through <kbd>&#8984;</kbd>+<kbd>8</kbd> to jump to a tab on Mac
+5. Press <kbd>Ctrl</kbd>+<kbd>1</kbd> through <kbd>Ctrl</kbd>+<kbd>8</kbd> to jump to a tab on Windows
+
+### Bonus ###
+Now with added keyboard shortcuts!
+
+Function | Shortcut
+--- | ---
+Toggle in current window| <kbd>Ctrl</kbd>+<kbd>Space</kbd>
+Toggle in all windows | <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>Space</kbd>
--- src/tumber.js
+++ src/tumber.js
@@ -1,6 +1,27 @@
 const SHOW = 'show';
 const HIDE = 'hide';
+const TOGGLE_CURRENT = 'toggle_current_window';
+const TOGGLE_ALL = 'toggle_all_windows';
 const TAB_LIMIT = 8;
+
+const activeWindowIds = new Set();
+let isActiveAllWindows = false;
+
+const showTabNumbersInAllWindows = () => {
+  chrome.windows.getAll({}, windows => {
+    for (let window of windows) {
+      showTabNumbersInWindow(window.id);
+    }
+    isActiveAllWindows = true;
+  });
+};
+
+const removeTabNumbersInAllWindows = () => {
+  for (let activeWindowId of activeWindowIds) {
+    removeTabNumbersInWindow(activeWindowId);
+  }
+  isActiveAllWindows = false;
+};
 
 const showTabNumbersInWindow = (windowId) => {
   chrome.tabs.query({ windowId }, tabs => {
@@ -11,6 +32,7 @@ const showTabNumbersInWindow = (windowId) => {
         { code: `document.title = "${tab.index + 1}: ${tab.title.substring(tab.title.indexOf(":") + 1)}";` }
       );
     }
+    activeWindowIds.add(windowId);
   });
 };
 
@@ -20,6 +42,7 @@ const removeTabNumbersInWindow = (windowId) => {
       tab.url.includes('http') &&
       chrome.tabs.executeScript(tab.id, { code: resetTabTitle(tab.title) })
     }
+    activeWindowIds.delete(windowId);
   });
 };
 
@@ -29,34 +52,47 @@ const resetTabTitle = (tabTitle) => {
     : `document.title = "${tabTitle}";`;
 };
 
-let activeWindows = [];
-
 const onClickHandler = (info) => {
   switch (info.menuItemId) {
     case SHOW:
-      showTabNumbersInWindow(chrome.windows.WINDOW_ID_CURRENT);
-      chrome.windows.getCurrent({}, window => activeWindows.push(window.id));
+      chrome.windows.getCurrent({}, window => showTabNumbersInWindow(window.id));
       break;
     case HIDE:
-      removeTabNumbersInWindow(chrome.windows.WINDOW_ID_CURRENT);
-      chrome.windows.getCurrent({}, window => {
-        activeWindows = activeWindows.filter(activeWindow => activeWindow !== window.id)
-      });
+      chrome.windows.getCurrent({}, window => removeTabNumbersInWindow(window.id));
       break;
     default:
       break;
   }
 };
 
+chrome.commands.onCommand.addListener((command) => {
+  switch (command) {
+    case TOGGLE_CURRENT:
+      chrome.windows.getCurrent({}, window => {
+        activeWindowIds.has(window.id)
+          ? removeTabNumbersInWindow(window.id)
+          : showTabNumbersInWindow(window.id)
+      });
+      break;
+    case TOGGLE_ALL:
+      isActiveAllWindows
+        ? removeTabNumbersInAllWindows()
+        : showTabNumbersInAllWindows()
+      break;
+    default:
+      break;
+  }
+});
+
 chrome.tabs.onMoved.addListener((tabId, moveInfo) => {
-  if (activeWindows.includes(moveInfo.windowId)) {
-    removeTabNumbersInWindow(chrome.windows.WINDOW_ID_CURRENT);
-    showTabNumbersInWindow(chrome.windows.WINDOW_ID_CURRENT);
+  if (activeWindowIds.has(moveInfo.windowId)) {
+    removeTabNumbersInWindow(moveInfo.windowId);
+    showTabNumbersInWindow(moveInfo.windowId);
   }
 });
 
 chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
-  if (activeWindows.includes(detachInfo.oldWindowId)) {
+  if (activeWindowIds.has(detachInfo.oldWindowId)) {
     showTabNumbersInWindow(detachInfo.oldWindowId);
     chrome.tabs.get(tabId, (tab) => {
       chrome.tabs.executeScript(tabId, { code: resetTabTitle(tab.title) })
@@ -65,15 +101,15 @@ chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
 });
 
 chrome.tabs.onAttached.addListener((tabId, attachInfo) => {
-  if (activeWindows.includes(attachInfo.newWindowId)) {
+  if (activeWindowIds.has(attachInfo.newWindowId)) {
     showTabNumbersInWindow(attachInfo.newWindowId);
   }
 });
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  if (activeWindows.includes(removeInfo.windowId)) {
-    removeTabNumbersInWindow(chrome.windows.WINDOW_ID_CURRENT);
-    showTabNumbersInWindow(chrome.windows.WINDOW_ID_CURRENT);
+  if (activeWindowIds.has(removeInfo.windowId)) {
+    removeTabNumbersInWindow(removeInfo.windowId);
+    showTabNumbersInWindow(removeInfo.windowId);
   }
 });
 
