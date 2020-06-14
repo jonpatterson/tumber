f6a92b102ca7654cc5b10c015658d749ce22c22b
Jon Patterson <jonpatterson@users.noreply.github.com>
2020-06-14 19:36:46 +0100

    Refactor webpack config to reduce duplicated code

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,48 @@ const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const metadata = require('./package.json');
 
-const CHROME = {
-  name: 'Tumber - Tab Numbers for Google Chrome\u2122',
-  description: 'Show tab numbers in Google Chrome\u2122 for tab jumping ninja!',
-};
-const EDGE = {
-  name: 'Tumber - Tab Numbers for Microsoft Edge\u2122',
-  description:
-    'Show tab numbers in Microsoft Edge\u2122 for tab jumping ninja!',
+const BROWSERS = {
+  chrome: {
+    name: 'Tumber - Tab Numbers for Google Chrome\u2122',
+    description:
+      'Show tab numbers in Google Chrome\u2122 for tab jumping ninja!',
+  },
+  edge: {
+    name: 'Tumber - Tab Numbers for Microsoft Edge\u2122',
+    description:
+      'Show tab numbers in Microsoft Edge\u2122 for tab jumping ninja!',
+  },
 };
 
-const optimize = (buffer, name, description) => {
-  const manifest = JSON.parse(buffer.toString());
-  manifest.version = metadata.version;
-  manifest.name = name;
-  manifest.description = description;
+const optimize = (buffer, platform) => {
+  const template = JSON.parse(buffer.toString());
+
+  const manifest = {
+    ...template,
+    version: metadata.version,
+    name: platform ? BROWSERS[platform].name : metadata.name,
+    description: platform
+      ? BROWSERS[platform].description
+      : metadata.description,
+  };
+
   return JSON.stringify(manifest, null, 2);
 };
 
-const copyPluginConfig = (platform) => {
-  switch (platform) {
-    case 'chrome':
-      return {
-        patterns: [
-          { from: 'src/icons', to: 'icons' },
-          {
-            from: path.resolve(__dirname, 'src/manifest.json'),
-            transform(content) {
-              return optimize(content, CHROME.name, CHROME.description);
-            },
-          },
-        ],
-      };
-    case 'edge':
-      return {
-        patterns: [
-          { from: 'src/icons', to: 'icons' },
-          {
-            from: path.resolve(__dirname, 'src/manifest.json'),
-            transform(content) {
-              return optimize(content, EDGE.name, EDGE.description);
-            },
-          },
-        ],
-      };
-  }
+const copyPluginConfig = (env) => {
+  return {
+    patterns: [
+      { from: 'src/icons', to: 'icons' },
+      {
+        from: path.resolve(__dirname, 'src/manifest.json'),
+        transform(content) {
+          return env.development
+            ? optimize(content)
+            : optimize(content, env.platform);
+        },
+      },
+    ],
+  };
 };
 
 module.exports = (env) => ({
@@ -55,27 +52,13 @@ module.exports = (env) => ({
   devtool: env.production ? 'none' : 'inline-source-map',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, `dist/${env.platform}`),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
   resolve: {
     extensions: ['*', '.js'],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin(copyPluginConfig(env.platform)),
-    new CopyPlugin({
-      patterns: [
-        { from: 'src/icons', to: 'icons' },
-        {
-          from: path.resolve(__dirname, 'src/manifest.json'),
-          transform(content) {
-            return optimize(content);
-          },
-        },
-      ],
-    }),
-  ],
+  plugins: [new CleanWebpackPlugin(), new CopyPlugin(copyPluginConfig(env))],
   module: {
     rules: [
       {
